b01f9665f901e2575bbfddbbbd51f303c159910b
Chris Feijoo <chris@feijoo.fr>
2018-04-07 01:37:27 +0200

    Create window using SDL2

--- Makefile
+++ Makefile
@@ -12,11 +12,11 @@ NAME=exmachina
 
 $(NAME): main.o
 	mkdir -p dist
-	cc -o dist/$(NAME) obj/main.o
+	cc -o dist/$(NAME) `sdl2-config --libs` obj/main.o
 
 main.o:
 	mkdir -p obj
-	cc -c src/main.c -o obj/main.o
+	cc -c src/main.c `sdl2-config --cflags` -o obj/main.o
 
 clean:
 	rm -rf exmachina
--- src/main.c
+++ src/main.c
@@ -8,10 +8,47 @@
      ## ## ## :##
       ## ## ##*/
 
+#include <stdlib.h>
 #include <stdio.h>
+#include "SDL.h"
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    printf("Hello World.\n");
-    return 0;
+	SDL_Event e;
+	SDL_Window *window;
+	int should_quit = 0;
+
+	if (SDL_Init(SDL_INIT_VIDEO) == -1)
+	{
+		printf("ERROR during SDL init: %s\n", SDL_GetError());
+	}
+
+	window = SDL_CreateWindow(
+		"ExMachina",
+		42, 42,
+		640, 480,
+		SDL_WINDOW_OPENGL);
+
+	if (window == NULL)
+	{
+		printf("ERROR during SDL window creation: %s\n", SDL_GetError());
+		return EXIT_FAILURE;
+	}
+
+	while (!should_quit)
+	{
+		while (SDL_PollEvent(&e))
+		{
+			switch (e.type)
+			{
+			case SDL_QUIT:
+				should_quit = 1;
+				break;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return EXIT_SUCCESS;
 }
